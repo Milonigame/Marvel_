96bf85cee79a97873f8885119d06f436d143a0ce
Mariia <milonigame1@gmail.com>
2023-11-10 19:33:00 +0300

    start

---
+++ src/services/MarvelService.js
@@ -0,0 +1,41 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=d5901d181ff17e71e1b465baca0a58ce';
+    _baseOffset=210;
+
+
+ getResource = async(url) =>{
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+ }
+
+ getAllCharacters = async (offset = this._baseOffset)=>{
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+ }
+ getCharacter = async(id)=>{
+  const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  return this._transformCharacter(res.data.results[0]);
+ }
+
+ _transformCharacter = (char) => {
+    return {
+      id: char.id,
+        name: char.name,
+        description: char.name.description,
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage:char.urls[0].url,
+        wiki: char.urls[1].url,
+        comics: char.comics.items.slice(0, 5)
+    }
+ }
+}
+
+
+export default MarvelService;
